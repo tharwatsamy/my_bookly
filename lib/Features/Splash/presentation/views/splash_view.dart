d86d0d70a8e0e085335bbcf427a7f0bae67dc531
tharwatsamy <tharwatsamy2004@gmail.com>
2022-09-23 04:41:38 +0200

    create splash view

---
+++ lib/Features/Splash/presentation/views/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'Features/Splash/presentation/views/splash_view.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const Bookly());
 }
 
 class Bookly extends StatelessWidget {
@@ -10,7 +12,7 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       home: SplashView(),
     );
   }
