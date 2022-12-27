ada7c670e868c674bbece256e388e8cc1ac6edcb
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2022-12-27 02:28:35 +0200

    newest books cubit done

--- lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
@@ -2,8 +2,21 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/repos/home_repo.dart';
+
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
