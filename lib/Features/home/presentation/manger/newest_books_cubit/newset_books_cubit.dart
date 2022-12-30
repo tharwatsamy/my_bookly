65193b4830adb479137d74b5a3133a40ed0b675a
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2022-12-30 03:41:25 +0200

    fix issue with loading data

--- lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
@@ -12,7 +12,7 @@ class NewsetBooksCubit extends Cubit<NewsetBooksState> {
   final HomeRepo homeRepo;
   Future<void> fetchNewestBooks() async {
     emit(NewsetBooksLoading());
-    var result = await homeRepo.fetchFeaturedBooks();
+    var result = await homeRepo.fetchNewsetBooks();
     result.fold((failure) {
       emit(NewsetBooksFailure(failure.errMessage));
     }, (books) {
--- lib/core/widgets/custom_error_widget.dart
+++ lib/core/widgets/custom_error_widget.dart
@@ -9,9 +9,11 @@ class CustomErrorWidget extends StatelessWidget {
   final String errMessage;
   @override
   Widget build(BuildContext context) {
-    return Text(
-      errMessage,
-      style: Styles.textStyle18,
+    return Center(
+      child: Text(
+        errMessage,
+        style: Styles.textStyle18,
+      ),
     );
   }
 }
